6174c5850bc7848eca6cbbfeea876ed432da01f4
dylanlpa <dylanhang2001@gmail.com>
2025-04-03 16:34:38 -0700

    Add files via upload
    added payment (not fully done w/ features)

--- frontend/app/src/main/java/com/example/test/Payment.java
+++ frontend/app/src/main/java/com/example/test/Payment.java
@@ -1,6 +1,9 @@
 package com.example.test;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Payment extends AppCompatActivity {
 
-
+    Button pay_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,17 @@ public class Payment extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        pay_btn = findViewById(R.id.pay_button);
+
+        pay_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Payment.this, CheckoutActivity.class);
+                startActivity(i);
+                finish();
+            }
         });
     }
 }
